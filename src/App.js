604c267dc46f03e67480ba4ad571c9559e081969
levilu800b <levilu800b@gmail.com>
2021-09-24 14:09:56 +0100

    chat on

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
 const App = () => {
+	if (!localStorage.getItem('username')) return <LoginForm />;
+
 	return (
 		<ChatEngine
 			height="100vh"
 			projectID="3ce2d852-9d61-4375-9f75-9761b2afad83"
-			userName="Jamespeters"
-			userSecret="12345"
+			userName={localStorage.getItem('username')}
+			userSecret={localStorage.getItem('password')}
 			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 		/>
 	);
